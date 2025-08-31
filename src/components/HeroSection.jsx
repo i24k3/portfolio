81742567054a7ff72b4a684e4959bfcadbefb03d
イざけ <akha3743@gmail.com>
2025-08-31 20:35:27 +0530

    refactor web content.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use strict";
 
 export const HeroSection = ({data}) => {
-    const {headline, subHeadline} = data;
+    const {headline, subHeadline, subHeadline1} = data;
     return (
         <section className="bg-white text-gray-900 py-44 border-amber-600">
             <div className="max-w-3xl mx-auto px-6 text-center">
@@ -10,6 +10,9 @@ export const HeroSection = ({data}) => {
                 </h1>
                 <p className="text-xl md:text-2xl text-gray-600 leading-relaxed">
                     {subHeadline}
+                </p>
+                <p className="mt-5 text-xl text-gray-500 font-bold px-1 box-decoration-clone">
+                    {subHeadline1}
                 </p>
             </div>
         </section>
